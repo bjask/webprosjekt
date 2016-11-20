ffea69febccc41679a3f28dea561b4c20b46bb68
bjask <bjarne.skurdal@gmail.com>
2016-11-20 23:08:28 +0100

    Fikset for-løkke på event listener :-)
    Det var noe med "closeure" som gjorde susen.

--- hyttevalgjavascript.js
+++ hyttevalgjavascript.js
@@ -52,8 +52,9 @@ function VelgHytte(hyttenr) {
       txt += "</table>";
       txt += '<iframe src="http://www.yr.no/sted/Norge/' + $(xml).find(hyttevaer).text() + //Værvarsel
       '/ekstern_boks_liten.html" width="180" height="322" frameborder="0" style="margin: 10px 0 10px 0" scrolling="no"></iframe>';
-
+      setTimeout(function(){
       $(".main").append(txt);  //Alt i variabelen txt skrives til hytte.html
+      }, 300);
     }
    });
    }
